28a24729014bad5b52e739451023e33996591606
Vitaliy Sergeychik <veesergey@gmail.com>
2020-07-24 12:52:59 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,90 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+provider "vault" {
+  address = "http://127.0.0.1:8200"
+  add_address_to_env = "true"
+}
+
+// Secret Engine, Issues the AWS access key and secret key.
+// The keys given used by this resource are the ones used the create the new IAM user. 
+resource "vault_aws_secret_backend" "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  path       = "aws-path"
+
+  default_lease_ttl_seconds = "120"
+  max_lease_ttl_seconds     = "240"
+}
+
+// The IAM User that actually creates the EC2 instance
+resource "vault_aws_secret_backend_role" "EC2_Creator" {
+  backend = "${vault_aws_secret_backend.aws.path}"
+  name    = "EC2Creator-role"
+  credential_type = "iam_user"
+  policy_document = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "iam:*", "ec2:*"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+// Reads the AWS Credentials for the EC2_Creator Role
+data "vault_aws_access_credentials" "creds" {
+  backend = "${vault_aws_secret_backend.aws.path}"
+  role    = "${vault_aws_secret_backend_role.EC2_Creator.name}"
+}
+
+provider "aws" {
+  access_key = "${data.vault_aws_access_credentials.creds.access_key}"
+  secret_key = "${data.vault_aws_access_credentials.creds.secret_key}"
+  region     = "us-east-1"
+}
+
+# Specifies whats being created. In this case its a linux EC2 instance.
+# It also adds a security group. Notice that the security group is added to the instance despite it being
+# defined further down. Terraform automatically figures out the relationship in dependencies and will know
+# that it must create the security group first, and then add it to the instance.
+
+resource "aws_instance" "linux2" {
+    ami = "ami-0a887e401f7654935"
+    instance_type = "t2.micro"
+    security_groups = ["allow_ssh_http"]
+    tags = {
+        Name = "Linux EC2"
+    }
+}
+
+# This is the creation of the security group. There are two outbound rules that are being created.
+# One rule allows all internet traffic connection, the other allows SSH connections
+resource "aws_security_group" "ssh_http" {
+    name = "allow_ssh_http"
+    description = "Allows incoming SSH connection to port 22 and http for port 80."
+
+  ingress {
+      description = "Allows SSH connections (linux)"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allows Internet traffic connections"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+}
